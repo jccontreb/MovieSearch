168ed2f5551053b43b6e44bf6f78aa90589e29e5
Jonatan Contreras <jonatan.contreras@vml.com>
2024-11-29 17:03:31 -0600

    Initial Commit

---
+++ MovieSearch/MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchApp.swift
+//  MovieSearch
+//
+//  Created by jcontreras on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
